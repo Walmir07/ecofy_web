49bc516d30a12fb34d3078189b7fc9ed860389ba
Walmir07 <walmir.lima@academico.ifpb.edu.br>
2025-07-23 21:45:01 -0300

    Incluindo campo de busca à tela de Home

--- frontend/src/pages/Home/Home.jsx
+++ frontend/src/pages/Home/Home.jsx
@@ -1,10 +1,18 @@
 import React from 'react';
 import "./Home.css";
+import  { Link } from "react-router-dom";
 
 const Home = () => {
   return (
-    <div className="container">
-        <h1>Home</h1>
+    <div className="home">
+        <div className='opcoes'>
+          <input className='barra-pesquisa' placeholder='Buscar planta...'></input>
+          <button className='botoes'>Pesquisar</button>
+          <Link className='botoes'>Adicionar</Link>
+        </div>
+        <div className='catalogo'>
+          <p>Aqui ficará o catálogo</p>
+        </div>
     </div>
   )
 }
