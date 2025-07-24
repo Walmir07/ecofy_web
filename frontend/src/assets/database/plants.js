9d3c27306126ab8e9ce8bcbd873a0dee98ab3e63
Walmir07 <walmir.lima@academico.ifpb.edu.br>
2025-07-23 21:48:51 -0300

    Adição de base de dados genérica para testes

---
+++ frontend/src/assets/database/plants.js
@@ -0,0 +1,23 @@
+export const plantas = [
+  {
+    _id: "683e3a10a429f73f128d946e",
+    nome: "Lírio da Paz",
+    categoria: "Ornamental",
+    descricao: "Planta elegante com flores brancas distintas, conhecida por purificar o ar e prosperar em ambientes com pouca luz.",
+    imagemUrl: "https://exemplo.com/imagens/lirio-da-paz.jpg"
+  },
+  {
+    _id: "683e3a10a429f73f128d946f",
+    nome: "Cacto Opuntia",
+    categoria: "Cactos",
+    descricao: "Variedade de cacto robusto e de fácil cultivo, com formato de raquete. Perfeito para ambientes ensolarados e secos.",
+    imagemUrl: "https://exemplo.com/imagens/cacto-opuntia.jpg"
+  },
+  {
+    _id: "683e3a10a429f73f128d9470",
+    "nome": "Pimenta Ornamental",
+    categoria: "Ornamental Comestível",
+    descricao: "Planta vibrante com pequenos frutos coloridos que adicionam um toque decorativo e podem ser consumidos (alguns picantes!).",
+    imagemUrl: "https://exemplo.com/imagens/pimenta-ornamental.jpg"
+  }
+]
