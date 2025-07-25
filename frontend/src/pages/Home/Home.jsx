7335bf6711e3749c1d3acf5c438e06c409bd7f95
Walmir07 <walmir.lima@academico.ifpb.edu.br>
2025-07-24 23:03:00 -0300

    Adição de Cards de plantas à tela de Home

--- frontend/src/pages/Home/Home.jsx
+++ frontend/src/pages/Home/Home.jsx
@@ -1,17 +1,26 @@
 import React from 'react';
 import "./Home.css";
 import  { Link } from "react-router-dom";
+import Card from '../../components/Card/Card.jsx';
+import { plantas } from '../../assets/database/plants.js';
 
 const Home = () => {
   return (
     <div className="home">
-        <div className='opcoes'>
-          <input className='barra-pesquisa' placeholder='Buscar planta...'></input>
-          <button className='botoes'>Pesquisar</button>
-          <Link className='botoes'>Adicionar</Link>
+         <div className='opcoes'>
+            <input className='barra-pesquisa' placeholder='Buscar planta...'></input>
+            <button className='botoes'>Pesquisar</button>
+            <Link className='botoes'>Adicionar</Link>
         </div>
         <div className='catalogo'>
-          <p>Aqui ficará o catálogo</p>
+            
+            {plantas.map(p => (
+              <Card
+                _id={p._id}
+                nome={p.nome}
+              ></Card>
+            ))}
+
         </div>
     </div>
   )
