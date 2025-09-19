86f18ee0efce7ac65976f3d7b7b5ceea59626512
Walmir07 <walmir.lima@academico.ifpb.edu.br>
2025-09-19 08:37:33 -0300

    Desenvolvendo campo de imagem com preview para cadastro de plantas

--- frontend/src/pages/Create/Create.jsx
+++ frontend/src/pages/Create/Create.jsx
@@ -1,9 +1,50 @@
-import React from 'react';
+import React, { useState } from 'react';
 import "./Create.css";
 import { Link } from 'react-router-dom';
 import imagemSeta from "/arrow-left.svg";
 
 const Create = () => {
+
+  const [imagem, setImagem] = useState(null);
+
+  const uploadImagemPlanta = (event) => {
+      const file = event.target.files[0];
+        if (file) {
+        const reader = new FileReader();
+        reader.onloadend = () => {
+          setImagem(reader.result); // Atualiza o estado com a imagem carregada
+        };
+        reader.readAsDataURL(file); // Lê o arquivo como uma URL de dados
+    }
+  };
+
+  const addPlanta = async (novaPlanta) => {
+  const response = await fetch('/api/plantas', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(novaPlanta),
+  });
+};
+
+const submeterPlanta = (e) => {
+  e.preventDefault();
+  const novaPlanta = {
+
+    nomeProjeto, // 1 - Adicionar dados das plantas
+
+  };
+
+  console.log(novaPlanta);
+
+  addPlanta(novaPlanta);
+  
+  setImagem(null); // 2 - Adicionar os sets dos dados
+
+}
+
+
   return (
     <div className='create'>
         <div className="area-voltar">
@@ -13,7 +54,7 @@ const Create = () => {
           </Link>
         </div>
         <div className='area-formulario'>
-            <div className='formulario'>
+            <div className='formulario' onSubmit={submeterPlanta}>
                 <div className="area-inputs">
                     <div className="dados">
                         <input type='text' placeholder='Nome da planta...'></input>
@@ -22,7 +63,19 @@ const Create = () => {
                     </div>
                     <div className="imagem">
                         <p>Imagem</p>
-                        <input type="file" />
+
+                        <label htmlFor='imagemPlanta' className='imagem-planta'>
+                            <input 
+                                type='file'
+                                id='imagemPlanta'
+                                name='imagem'
+                                accept='image/*'
+                                onChange={uploadImagemPlanta}
+                            />
+                            <span className="add-image">{imagem ? "" : "+"}</span>
+                            {imagem && <img src={imagem} alt="Uploaded" />}
+                        </label>
+
                     </div>
                 </div>
                 <div className="area-salvar">
